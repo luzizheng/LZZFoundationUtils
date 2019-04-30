f71a1a2299c0a06f7cfa8da81006377d6670a9d2
Frank Lu <arloou@gmail.com>
2019-04-30 09:54:44 +0800

    add podspec file
    add podspec file

---
+++ LZZFoundationUtilsHost/LZZFoundationUtils/LZZFUTools.h
@@ -0,0 +1,17 @@
+//
+//  LZZFUTools.h
+//  LZZFoundationUtilsHost
+//
+//  Created by 玩车tv on 2019/4/30.
+//  Copyright © 2019 玩车tv. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LZZFUTools : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LZZFoundationUtilsHost/LZZFoundationUtils/LZZFUTools.m
@@ -0,0 +1,13 @@
+//
+//  LZZFUTools.m
+//  LZZFoundationUtilsHost
+//
+//  Created by 玩车tv on 2019/4/30.
+//  Copyright © 2019 玩车tv. All rights reserved.
+//
+
+#import "LZZFUTools.h"
+
+@implementation LZZFUTools
+
+@end
