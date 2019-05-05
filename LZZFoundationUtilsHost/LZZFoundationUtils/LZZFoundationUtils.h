e5535c75f11e08c38562c35c4d1ab9e638f6794a
Frank Lu <arloou@gmail.com>
2019-05-05 10:06:44 +0800

    add liscense file
    add liscense file

---
+++ LZZFoundationUtilsHost/LZZFoundationUtils/LZZFoundationUtils.h
@@ -0,0 +1,19 @@
+//
+//  LZZFoundationUtils.h
+//  LZZFoundationUtilsHost
+//
+//  Created by 玩车tv on 2019/4/30.
+//  Copyright © 2019 玩车tv. All rights reserved.
+//
+
+#ifndef LZZFoundationUtils_h
+#define LZZFoundationUtils_h
+
+
+#endif /* LZZFoundationUtils_h */
+
+
+#import "LZZFUTools.h"
+
+
+
